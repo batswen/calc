b374736a7686fcde63ce5b978e99036a42d61f3d
batswen <batswen@gmail.com>
2019-10-18 17:10:42 +0200

    Init upload

---
+++ js/calc.js
@@ -0,0 +1,88 @@
+var output = document.getElementById("calc-output")
+var input = "", parens = 0, error = false
+
+function calc_show() {
+    console.log(input)
+}
+
+function calc_clear() {
+    output.innerHTML = "0"
+    input = ""
+    error = false
+    calc_show()
+}
+
+
+ function calc_error() {
+     error = true
+     output.innerHTML = "Error"
+ }
+
+function calc_concat(e) {
+    var num = Number(output.innerHTML)
+    if (error) {
+        return
+    }
+    // Allow only one dot
+    if (e == "." && output.innerHTML.indexOf(".") != -1) {
+        calc_error()
+    }
+    // Add new element to output
+    output.innerHTML = output.innerHTML + e
+    input += e
+    // Remove leading zeros
+    while (output.innerHTML.charAt(0) == "0" && output.innerHTML.charAt(1) != ".") {
+        output.innerHTML = output.innerHTML.slice(1)
+    }
+    if (output.innerHTML == "") {
+        output.innerHTML = "0"
+    }
+    calc_show()
+}
+
+function calc_backspace() {
+    if (error) {
+        return
+    }
+    output.innerHTML = output.innerHTML.slice(0, -1)
+    input = input.slice(0, -1)
+    if (output.innerHTML == "") {
+        output.innerHTML = "0"
+    }
+    calc_show()
+}
+
+function calc_operator(operator) {
+    if (error) {
+        return
+    }
+    if (operator == "(") {
+        parens++
+        input += "("
+    } else if (operator == ")") {
+        if (parens == 0) {
+            return calc_error()
+        }
+        parens--
+        input += ")"
+    } else {
+        input += operator
+    }
+    output.innerHTML = "0"
+    calc_show()
+}
+
+function calc_eval() {
+    if (error) {
+        return
+    }
+    calc_show()
+    if (input.length > 0) {
+        var l = new Lexer(input)
+        var i = new Interpreter(l)
+        document.getElementById("calc-result").innerHTML = i.expr()
+    }
+    calc_clear()
+}
+
+calc_show()
---
+++ js/evaluator.js
@@ -0,0 +1,162 @@
+/*
+    Based on "Let’s Build A Simple Interpreter" by Ruslan Spivak
+    https://ruslanspivak.com/lsbasi-part1/
+
+    Translated from Python source
+*/
+
+const EOF = 0, NUMBER = 1, PLUS = 2, MINUS = 3, MUL = 4, DIV = 4, LPAREN = 5, RPAREN = 6,
+    EXP = 7, SQRT = 8, MOD = 9
+
+class Lexer {
+    constructor(source) {
+        this.source = source
+        this.pos = 0
+        this.chr = this.source.charAt(this.pos)
+    }
+    error(msg) {
+        alert("Lexer > error: "+msg)
+    }
+    getNextChar() {
+        this.pos++
+        if (this.pos >= this.source.length) {
+            this.chr = undefined
+        } else {
+            this.chr = this.source.charAt(this.pos)
+        }
+    }
+    getNumber() {
+        var result = ""
+        while (this.chr != undefined && this.chr >= "0" && this.chr <= "9") {
+            result += this.chr
+            this.getNextChar()
+        }
+        if (this.chr != undefined && this.chr == ".") {
+            result += "."
+            this.getNextChar()
+            while (this.chr != undefined && this.chr >= "0" && this.chr <= "9") {
+                result += this.chr
+                this.getNextChar()
+            }
+        }
+        return result
+    }
+    getNextToken() {
+        while (this.chr != undefined) {
+            if (this.chr >= "0" && this.chr <= "9" || this.chr == ".") {
+                return { type: NUMBER, value: this.getNumber() }
+            }
+            if (this.chr == "+") {
+                this.getNextChar()
+                return { type: PLUS, value: "+" }
+            }
+            if (this.chr == "-") {
+                this.getNextChar()
+                return { type: MINUS, value: "-" }
+            }
+            if (this.chr == "*") {
+                this.getNextChar()
+                return { type: MUL, value: "*" }
+            }
+            if (this.chr == "/") {
+                this.getNextChar()
+                return { type: DIV, value: "/" }
+            }
+            if (this.chr == "(") {
+                this.getNextChar()
+                return { type: LPAREN, value: "(" }
+            }
+            if (this.chr == ")") {
+                this.getNextChar()
+                return { type: RPAREN, value: ")" }
+            }
+            if (this.chr == "^") {
+                this.getNextChar()
+                return { type: EXP, value: "^" }
+            }
+            if (this.chr == "@") {
+                this.getNextChar()
+                return { type: SQRT, value: "Sqrt" }
+            }
+            if (this.chr == "%") {
+                this.getNextChar()
+                return { type: MOD, value: "%" }
+            }
+            this.error("getNextToken(): unknown token")
+        }
+        return { type: EOF, value: "" }
+    }
+}
+
+class Interpreter {
+    constructor(lexer) {
+        this.lexer = lexer
+        this.token = this.lexer.getNextToken()
+    }
+    error(msg) {
+        alert("Interpreter > error: "+msg)
+    }
+    eat(tokentype) {
+        if (this.token.type == tokentype) {
+            this.token = this.lexer.getNextToken()
+        } else {
+            this.error("Wrong tokentype (expected: "+tokentype+", found: "+this.token.type+")")
+        }
+    }
+    /*
+    * number | ( expr )
+    */
+    factor() {
+        var token = this.token, result
+        if (token.type == NUMBER) {
+            this.eat(NUMBER)
+            result = token.value
+
+            return result
+        } else if (token.type == LPAREN) {
+            this.eat(LPAREN)
+            result = this.expr()
+            this.eat(RPAREN)
+            return result
+        }
+    }
+    /**
+    ** factor (*|/|^|%) factor
+    */
+    term() {
+        var result = this.factor()
+        while (this.token.type == MUL || this.token.type == DIV ||
+            this.token.type == EXP || this.token.type == MOD) {
+            if (this.token.type == MUL) {
+                this.eat(MUL)
+                result *= this.factor()
+            } else if (this.token.type == DIV) {
+                this.eat(DIV)
+                result /= this.factor()
+            } else if (this.token.type == EXP) {
+                this.eat(EXP)
+                result = Math.pow(result, this.factor())
+            } else {
+                this.eat(MOD)
+                result %= this.factor()
+            }
+        }
+        return result
+    }
+    /**
+     ** term (+|-) term
+     **/
+    expr() {
+        var result = this.term()
+        while (this.token.type == PLUS || this.token.type == MINUS) {
+            if (this.token.type == PLUS) {
+                this.eat(PLUS)
+                result = Number(result) + Number(this.term()) // ... javascript
+            } else {
+                this.eat(MINUS)
+                result -= this.term()
+            }
+        }
+        return result
+    }
+}
